2ed61b490fa906a1588f24e2c8387bc133f4e395
Jacob Stokes-Warner <jacobstokeswarner@gmail.com>
2023-02-17 12:52:53 -0600

    added functionality that would allow the page to only generate random dice if it is reloaded, not if it is just loaded once

--- js/index.js
+++ js/index.js
@@ -1,21 +1,35 @@
-var randomNumber1 = (Math.floor((Math.random() * 6))+1)
-var randomNumber2 = (Math.floor((Math.random() * 6))+1)
+const pageAccessedByReload = (
+    (window.performance.navigation && window.performance.navigation.type === 1) ||
+      window.performance
+        .getEntriesByType('navigation')
+        .map((nav) => nav.type)
+        .includes('reload')
+  );
 
 
 
 
-var img1 = document.querySelector("img.img1");
+function randomDice() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-img1.setAttribute("src","images/dice" + randomNumber1 + ".png");
+  var img1 = document.querySelector("img.img1");
 
-var img2 = document.querySelector("img.img2");
+  img1.setAttribute("src", "images/dice" + randomNumber1 + ".png");
 
-img2.setAttribute("src","images/dice" + randomNumber2 + ".png");
+  var img2 = document.querySelector("img.img2");
 
-if (randomNumber1 > randomNumber2){
-    document.querySelector(".container h1").textContent = "Player 1 Wins!"
-} else if(randomNumber1 < randomNumber2){
-    document.querySelector(".container h1").textContent = "Player 2 Wins!"
-} else{
-    document.querySelector(".container h1").textContent = "Draw!"
+  img2.setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector(".container h1").textContent = "Player 1 Wins!";
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector(".container h1").textContent = "Player 2 Wins!";
+  } else {
+    document.querySelector(".container h1").textContent = "Draw!";
+  }
+}
+
+if(pageAccessedByReload){
+    randomDice();
 }
